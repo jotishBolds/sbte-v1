924b48dc5117bce4908342aad021605336b74d84
jotish03 <jotishsharma03@gmail.com>
2024-10-07 17:08:49 +0530

    fixed and added api programs

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,6 @@ export default async function RootLayout({
           <Navbar />
           {children}
           <Toaster />
-          <Footer />
         </Providers>
       </body>
     </html>
