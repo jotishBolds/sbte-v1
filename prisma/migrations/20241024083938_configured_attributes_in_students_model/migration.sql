11c0c88c4566a67a16e9c6f45b69df111daeedb9
shankhalp13 <shankhalppradhan2003@gmail.com>
2024-10-24 15:19:44 +0530

    Added the Student POST

---
+++ prisma/migrations/20241024083938_configured_attributes_in_students_model/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Student" ALTER COLUMN "enrollmentNo" DROP NOT NULL,
+ALTER COLUMN "guardianName" DROP NOT NULL,
+ALTER COLUMN "guardianGender" DROP NOT NULL,
+ALTER COLUMN "guardianMobileNo" DROP NOT NULL,
+ALTER COLUMN "guardianRelation" DROP NOT NULL;
