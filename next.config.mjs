4eb0df321503d76775f198e5a021abd4789a062f
jotish03 <jotishsharma03@gmail.com>
2025-03-24 22:12:56 +0530

    added headers in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
 
+    // Add this to allow loading from unpkg
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
