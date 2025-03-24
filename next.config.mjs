03d28d386fea2abb37590a9aeaea2f102d462827
jotish03 <jotishsharma03@gmail.com>
2025-03-24 21:21:38 +0530

    fixed cors error

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,25 @@ const nextConfig = {
   async headers() {
     return [
       {
+        // Matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+      // You can keep these specific routes if you need different settings for them,
+      // but they’ll be overridden by the more general "/api/:path*" rule above
+      {
         source: "/api/eligibilityList/:path*",
         headers: [
           { key: "Access-Control-Allow-Origin", value: "*" },
