d97631b842f813e30443d24b3b24bd228a5b6a38
jotish03 <jotishsharma03@gmail.com>
2025-03-25 23:50:16 +0530

    fixed double close icon

--- components/automated-examfee-insertion/examfee-insertion.tsx
+++ components/automated-examfee-insertion/examfee-insertion.tsx
@@ -137,9 +137,7 @@ export function AutoBaseExamFeeInsertion({
           <DialogClose
             className="absolute top-0 right-0 p-2 hover:bg-secondary rounded-full"
             aria-label="Close"
-          >
-            <X className="h-4 w-4" />
-          </DialogClose>
+          ></DialogClose>
         </DialogHeader>
 
         <div className="space-y-4 flex-grow overflow-auto">
